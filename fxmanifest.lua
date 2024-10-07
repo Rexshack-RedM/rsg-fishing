3159857ba4ca9d0a05553c9438eb4261b049a22d
RexshackGaming <rexshackuk@gmail.com>
2024-10-07 05:53:07 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'rsg-fishing'
+version '1.0.2'
+
+shared_scripts {
+    '@rsg-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
+}
+
+client_scripts {
+    'client/client_js.js',
+    'client/client.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+    'server/versionchecker.lua'
+}
+
+dependencies {
+    'rsg-core',
+    'ox_lib'
+}
+
+exports {
+    'GET_TASK_FISHING_DATA',
+    'SET_TASK_FISHING_DATA',
+    'VERTICAL_PROBE'
+}
+
+lua54 'yes'
